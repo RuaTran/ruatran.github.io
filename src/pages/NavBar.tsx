a166f029ba5401b064df9d381245637a7c9061bf
Christopher Tran <110074141+RuaTran@users.noreply.github.com>
2025-03-26 15:43:16 -0700

    CSS test fix
    flagged color for project card headers as important to fix browser rendering issue

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -87,7 +87,7 @@ export default function NavBar() {
               <Link fontSize='1vw' padding={[0, 0, 0, 0]} href='https://drive.google.com/uc?export=download&id=1RMHPRTLswuDfFIynJGyQBdaTd9p4WRMm' _hover={{ textDecoration: 'none' }} >Resume</Link>
             </Button>}
             
-            <IconButton className='hamburger-icon' _hover={{ bg: "rgba(67, 27, 92, 0.4)" }} borderColor='#431b5c' bg={'transparent'} variant='outline' size={'lg'}
+            <IconButton color="#FFFFFF" _hover={{ bg: "rgba(67, 27, 92, 0.4)" }} borderColor='#431b5c' bg={'transparent'} variant='outline' size={'lg'}
               icon={isOpen ? <CloseIcon /> : <HamburgerIcon />} aria-label={'Open Menu'} display={{ md: 'none' }} onClick={isOpen ? onClose : onOpen} />
           </Flex>
         </Flex>
